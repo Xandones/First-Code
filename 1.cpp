b3b122e16ad95c502f6549d02912c6ca03531c94
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2023-09-27 12:11:11 -0300

    Comments corrected after review of the class.

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-// The line above is the preprocessor. It adds the headers files to the code. 
+// The line above is the preprocessor. It adds the headers file iostream to the code. 
 
-int main() //Main function. The other ones will be called from this one.
+int main() //Main function. The other ones will be called from this one. Most C++ programs have this function.
 { // Begining of a code block. 
 	std::cout << "First code written by me!" << std::endl;
 	/*
 		std -> allows us to use the standard functions of C++.
-		We must use the quotation marks to write the string that will be displayed on the screen.
-		endl -> jumps to a new line.
+		cout -> data output command. It prints on the standard output (screen) the message specified within quotation marks because it's a string.
+		endl -> inserts a line breaker in the string.
 	*/
 	system("PAUSE");
 	return 0;
 	/*
 		system -> allows us to use the System Functions, related to the O.S.
 		PAUSE -> Pauses the Command Prompt window and asks for a random key to unpause it.
-		return 0 -> Returns to the standard O.S. if the code runned without errors.
+		return 0 -> Returns to the Windows shell if the code runned without errors.
 	*/
 } // End of a code block.
-// Part of Marcos Pacheco's C++ course in Udemy.
+
+  // Part of Marcos Pacheco's C++ course in Udemy.
